2de212bf5fe61b25eb85a6751a0f6b920995b9c2
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-27 20:35:10 +0530

    Create Simple Linked List

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Linked List Program");
+            LinkedList linked = new LinkedList();
+            linked.Add(40);
+            linked.Add(60);
+            linked.Add(50);
+
+            linked.Display();
         }
 
 
